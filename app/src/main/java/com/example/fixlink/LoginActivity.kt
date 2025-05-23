660218ce9ffb995ecf73d1799648fd806987aabc
juze23 <joseemcruz23@gmail.com>
2025-05-23 17:36:49 +0100

    feat: login page

--- app/src/main/java/com/example/fixlink/LoginActivity.kt
+++ app/src/main/java/com/example/fixlink/LoginActivity.kt
@@ -1,35 +1,20 @@
 package com.example.fixlink
 
-import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
-
-    private val splashTimeOut: Long = 2000
-
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        Handler(Looper.getMainLooper()).postDelayed({
-            // Create an Intent to start LoginActivity
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
-
-            // Close this activity so user can't go back to splash screen
-            finish()
-        }, splashTimeOut)
     }
 }
