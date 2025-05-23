a7686ed8a271bb7ba0abb7cc1d147b80ebb527cb
Joel Filipe Martins Amaral <131992612+Kng1011@users.noreply.github.com>
2025-05-24 00:14:52 +0100

    Forgot Password Page

--- app/src/main/java/com/example/fixlink/ForgotPasswordActivity.kt
+++ app/src/main/java/com/example/fixlink/ForgotPasswordActivity.kt
@@ -1,19 +1,21 @@
 package com.example.fixlink
 
 import android.content.Intent
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class LoginActivity : AppCompatActivity() {
-
+class ForgotPasswordActivity : AppCompatActivity() {
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_forgot_password)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -24,19 +26,12 @@ class LoginActivity : AppCompatActivity() {
     }
 
     private fun setupClickListeners() {
-        val signUpLink = findViewById<TextView>(R.id.sign_up_link)
-        val forgotPassword = findViewById<TextView>(R.id.forgot_password)
+        val backArrow = findViewById<ImageView>(R.id.back_arrow)
 
-        signUpLink.setOnClickListener {
-            val intent = Intent(this, SignUpActivity::class.java)
-            startActivity(intent)
+        backArrow.setOnClickListener {
+            finish() // Encerra a activity e volta para a anterior
         }
-
-        forgotPassword.setOnClickListener{
-            val intent = Intent(this, ForgotPasswordActivity::class.java)
-            startActivity(intent)
-        }
-
 
     }
+
 }
