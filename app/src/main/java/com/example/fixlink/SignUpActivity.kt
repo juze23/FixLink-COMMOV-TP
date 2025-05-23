7abb7b5a6df477244e1ca9693e0e05a4a5fa27e1
Joel Filipe Martins Amaral <131992612+Kng1011@users.noreply.github.com>
2025-05-23 18:54:27 +0100

    SignUp page

--- app/src/main/java/com/example/fixlink/SignUpActivity.kt
+++ app/src/main/java/com/example/fixlink/SignUpActivity.kt
@@ -1,34 +1,20 @@
 package com.example.fixlink
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class LoginActivity : AppCompatActivity() {
-
+class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_sign_up)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        setupClickListeners()
-    }
-
-    private fun setupClickListeners() {
-        val signUpLink = findViewById<TextView>(R.id.sign_up_link)
-
-        signUpLink.setOnClickListener {
-            val intent = Intent(this, SignUpActivity::class.java)
-            startActivity(intent)
         }
     }
 }
